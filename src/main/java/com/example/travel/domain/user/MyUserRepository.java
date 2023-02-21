9411b672bcae28203c598c80b3c19a5d87479b22
seeker1207 <jm3420@naver.com>
2023-02-21 13:58:34 +0900

    Feat: 유저 레포지토리 구현

--- src/main/java/com/example/travel/domain/user/MyUserRepository.java
+++ src/main/java/com/example/travel/domain/user/MyUserRepository.java
@@ -1,6 +1,8 @@
 package com.example.travel.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MyUserRepository extends JpaRepository<MyUser, Long> {
 }
