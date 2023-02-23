7024e6bd2e3003cbc6b8dedad60acae2a4a7bd72
seeker1207 <jm3420@naver.com>
2023-02-23 15:15:18 +0900

    Feat: 사용자별 도시목록 조회 API 구현 사용자별 여행중인 도시, 여행이 예정된 도시 쿼리작성 및 Repository 메서드 구현

--- src/main/java/com/example/travel/domain/city/CityRepository.java
+++ src/main/java/com/example/travel/domain/city/CityRepository.java
@@ -1,11 +1,32 @@
 package com.example.travel.domain.city;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
     Optional<City> findByCityName(String CityName);
+
+    @Query("select city  from City city " +
+            "join city.travels cityTravel " +
+            "join cityTravel.travel travel " +
+            "where travel.traveler.id = :userId " +
+            "and current date >= travel.startDate and current date <= travel.endDate " +
+            "order by travel.startDate"
+    )
+    Optional<List<City>> findTravelingCitiesByUser(Long userId);
+
+    @Query("select distinct city, min(travel.startDate) from City city " +
+            "join city.travels cityTravel " +
+            "join cityTravel.travel travel " +
+            "where travel.traveler.id = :userId " +
+            "and local_date < travel.startDate " +
+            "order by min(travel.startDate) asc limit 10"
+    )
+    Optional<List<City>> findWillTravelCitiesByUser(Long userId);
+
 }
