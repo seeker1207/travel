24c39b1c2b6fa55c52b0ed6820a10f9f9126b23b
seeker1207 <jm3420@naver.com>
2023-02-23 15:16:10 +0900

    TEST: 사용자별 도시목록 조회 API 테스트 사용자별 여행중인 도시, 여행이 예정된 도시 조회 API 테스트케이스 작성

--- src/test/java/com/example/travel/domain/city/CityRepositoryTest.java
+++ src/test/java/com/example/travel/domain/city/CityRepositoryTest.java
@@ -1,14 +1,15 @@
 package com.example.travel.domain.city;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
+import com.example.travel.domain.travel.Travel;
+import com.example.travel.domain.travel.TravelRepository;
+import com.example.travel.domain.user.MyUser;
+import com.example.travel.domain.user.MyUserRepository;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,16 +21,14 @@ class CityRepositoryTest {
     @Autowired
     private CityRepository cityRepository;
 
+    @Autowired
+    private MyUserRepository myUserRepository;
+
+    @Autowired
+    private TravelRepository travelRepository;
+
     private final String cityName = "부산";
     private final String desc = "부산은 바다가 아름다운 도시입니다.";
-
-    private City initCity;
-
-    @BeforeEach
-    void init(TestInfo info) {
-        initCity = City.builder().cityName(cityName).desc(desc).build();
-        cityRepository.save(initCity);
-    }
 
     @Test
     void 도시_정보가_저장된다() {
@@ -50,6 +49,8 @@ class CityRepositoryTest {
     @Test
     void 도시_정보가_조회된다() {
         //given
+        City savedCity = getCity(cityName, desc);
+        cityRepository.save(savedCity);
         //when
         City city = cityRepository.findByCityName(cityName).orElseThrow(NoSuchElementException::new);
         //then
@@ -61,6 +62,9 @@ class CityRepositoryTest {
     @Test
     void 도시_정보가_수정된다() {
         // given
+        City savedCity = getCity(cityName, desc);
+        cityRepository.save(savedCity);
+
         City city = cityRepository.findByCityName(cityName).orElseThrow(NoSuchElementException::new);
         String newCityName = "인천";
         String newDesc = "인천은 야경이 아름다운 도시입니다.";
@@ -68,18 +72,20 @@ class CityRepositoryTest {
         city.modifyCityNameAndDesc(newCityName, newDesc);
         cityRepository.save(city);
 
-        City savedCity = cityRepository.findByCityName(newCityName).orElseThrow(NoSuchElementException::new);
+        City updatedCity = cityRepository.findByCityName(newCityName).orElseThrow(NoSuchElementException::new);
 
         // then
-        assertEquals(savedCity.getCityName(), newCityName);
-        assertEquals(savedCity.getDesc(), newDesc);
+        assertEquals(updatedCity.getCityName(), newCityName);
+        assertEquals(updatedCity.getDesc(), newDesc);
     }
 
     @Test
     void 도시_정보가_삭제된다() {
         // given
+        City targetCity = getCity(cityName, desc);
+        cityRepository.save(targetCity);
         // when
-        cityRepository.delete(initCity);
+        cityRepository.delete(targetCity);
         // then
         assertThrows(NoSuchElementException.class,
                 () -> cityRepository.findByCityName(cityName).orElseThrow(NoSuchElementException::new)
@@ -87,4 +93,82 @@ class CityRepositoryTest {
 
     }
 
+    @Test
+    void 현재_여행중인_도시정보를_조회한다() {
+        //given
+        List<Travel> travels = makeAndSaveTravels();
+
+        //when
+        List<City> cities = cityRepository.findTravelingCitiesByUser(travels.get(0).getTraveler().getId())
+                .orElseThrow(NoSuchElementException::new);
+
+        //then
+        assertEquals(cities.get(0).getCityName(), "여행 중인 도시");
+    }
+
+    @Test
+    void 여행이_예정된_도시를_조회한다() {
+        //given
+        List<Travel> travels = makeAndSaveTravels();
+
+        //when
+        List<City> cities = cityRepository.findWillTravelCitiesByUser(travels.get(0).getTraveler().getId())
+                .orElseThrow(NoSuchElementException::new);
+
+        //then
+        assertEquals(cities.get(0).getCityName(), "여행 예정인 도시");
+    }
+
+
+    private City getCity(String cityName, String desc) {
+        return City.builder().cityName(cityName).desc(desc).build();
+    }
+
+    private MyUser getUser() {
+        return MyUser.builder().email("test@travel.com").password("1234").nickname("여행덕후").build();
+    }
+
+    private List<Travel> makeAndSaveTravels() {
+        Travel travelOne = Travel.builder()
+                .travelTitle("여행 중인 일정")
+                .startDate(LocalDate.now().minusDays(3))
+                .endDate(LocalDate.now().plusDays(3))
+                .build();
+
+        Travel travelTwo = Travel.builder()
+                .travelTitle("여행 예정인 일정")
+                .startDate(LocalDate.now().plusDays(3))
+                .endDate(LocalDate.now().plusDays(5))
+                .build();
+
+        Travel travelThree = Travel.builder()
+                .travelTitle("여행이 끝난 일정")
+                .startDate(LocalDate.now().minusDays(3))
+                .endDate(LocalDate.now().minusDays(1))
+                .build();
+
+        List<Travel> travels = List.of(travelOne, travelTwo, travelThree);
+
+        City cityOne = getCity("여행 중인 도시", "여행중인 도시입니다.");
+        City cityTwo = getCity("여행 예정인 도시", "여행예정인 도시입니다.");
+        City cityThree = getCity("여행이 끝난 도시", "여행이 끝난 도시입니다.");
+
+        List<City> cities = List.of(cityOne, cityTwo, cityThree);
+
+        MyUser myUser = getUser();
+        myUserRepository.save(myUser);
+
+        cities.forEach((city) -> cityRepository.save(city));
+        travelOne.addCity(cityOne);
+        travelTwo.addCity(cityTwo);
+        travelThree.addCity(cityThree);
+
+        travels.forEach((travel) -> {
+            travel.setTraveler(myUser);
+        });
+
+        travelRepository.saveAll(travels);
+
+        return travels;
+    }
 }
