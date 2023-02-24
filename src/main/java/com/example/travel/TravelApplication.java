e93cf9ef6b338604880b2417ecf2d0eced0f647b
seeker1207 <jm3420@naver.com>
2023-02-24 22:26:10 +0900

    Refactor: JpaAuditing 어노테이션 따로 호출하여 컨트롤러테스트 에러 방지

--- src/main/java/com/example/travel/TravelApplication.java
+++ src/main/java/com/example/travel/TravelApplication.java
@@ -2,9 +2,8 @@ package com.example.travel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+
 @SpringBootApplication
 public class TravelApplication {
 
