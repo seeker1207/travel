75f7ad22dfa98f468183b7f5d13e3f18a0cbcf33
seeker1207 <jm3420@naver.com>
2023-02-24 12:38:43 +0900

    Test: 엔티티 생성하는 Util함수 생성

---
+++ src/test/java/com/example/travel/domain/util/TestUtil.java
@@ -0,0 +1,26 @@
+package com.example.travel.domain.util;
+
+import com.example.travel.domain.city.City;
+import com.example.travel.domain.travel.Travel;
+import com.example.travel.domain.user.MyUser;
+
+import java.time.LocalDate;
+
+public class TestUtil {
+    static public City getCity() {
+        return City.builder().id(1L).cityName("부산").desc("바다가 아름다운 도시입니다.").build();
+    }
+
+    static public Travel getTravel() {
+        return Travel.builder()
+                .travelTitle("졸업 기념 여행")
+                .startDate(LocalDate.of(2023, 2, 1))
+                .endDate(LocalDate.of(2023, 2, 15))
+                .build();
+    }
+
+    static public MyUser getUser() {
+        return MyUser.builder().email("test@email.com").nickname("여행덕후").build();
+    }
+
+}
